3569bf085daee4a7f1df715c9ef95c3038d57e46
Vadim136 <barkal.bk@yandex.ru>
2024-09-25 20:35:34 +0300

    second commit

---
+++ task_1_2.py
@@ -0,0 +1,5 @@
+quantity_made_home_work = 12
+number_of_hours_spent = 1.5
+course_name = "Python"
+time_for_one_task = number_of_hours_spent/quantity_made_home_work
+print("Курс: ", course_name, "; Всего задач: ", quantity_made_home_work, "; Затрачено часов: ", number_of_hours_spent, "; Среднее время выполнения ", time_for_one_task, "часа.")
