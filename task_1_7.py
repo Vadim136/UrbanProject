77f303a3fb253d56b31efe10188b723b7b1befa6
Vadim136 <barkal.bk@yandex.ru>
2024-10-08 12:10:24 +0300

    correction module 1 task 7

--- task_1_7.py
+++ task_1_7.py
@@ -1,7 +1,7 @@
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
 
-list_students =list(students)
+list_students = sorted(students)
 
 arithmetic_mean_dict = {}
 
