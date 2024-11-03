fcd66202d3f121993ec3e4b64f13bcff7870ec4e
Vadim136 <barkal.bk@yandex.ru>
2024-11-03 20:16:25 +0300

    fixed task 1 modele 5

--- module 5/task_1.py
+++ module 5/task_1.py
@@ -5,9 +5,11 @@ class House:
         self.number_of_floors = number_of_floor
 
     def go_to(self, new_floor):
-        if new_floor > self.number_of_floors:
+        if new_floor > self.number_of_floors or new_floor < 1:
             print(f'Такого этажа нет в "{self.name}"')
         else:
+            for i in range(1, new_floor):
+                print(i)
             print(f'Вы на {new_floor} в "{self.name}"')
 
 
@@ -15,4 +17,4 @@ h1 = House('ЖК Горский', 18)
 h2 = House('Домик в деревне', 2)
 h1.go_to(5)
 h2.go_to(10)
-h1.go_to(18)
+h1.go_to(15)
